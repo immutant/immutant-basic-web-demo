94bf85d865af4e2a99d74317302a3d0dc5e9e89d
Tobias Crawley <tcrawley@gmail.com>
2012-07-17 10:52:06 -0400

    Minor updates.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject immutant-demo "1.0.0-SNAPSHOT"
   :description "A basic demo"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]]
   :immutant {:context-path "/"})
